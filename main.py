4fdc201bc3f8cc12b02e09c69e7b5f033d9a2cfd
PyDevIll <delphadyth@gmail.com>
2024-07-02 00:35:52 +0700

    Made FastAPI app with GET and POST operations. ISSUE: Cannot run task_worker concurrently

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uuid
+from pydantic import BaseModel
+
+app = FastAPI()
+tasks = {}      # хранит статусы задач
+
+
+class TaskModel(BaseModel):
+    duration: int
+
+
+async def task_worker(task_id, duration):
+    await asyncio.sleep(duration)
+    tasks[task_id] = "done"
+
+
+#            параметры в запросе POST попадают в параметры функции, в соответствии с TaskModel
+@app.post("/task", response_model=dict)
+async def create_task(task_model: TaskModel):
+    task_id = str(uuid.uuid4())
+    tasks[task_id] = "running"
+#            asyncio.create_task используется для создания объекта asyncio.Task из корутины,
+#            чтобы можно было запускать ее параллельно, а не ждать его завершения
+    _task = asyncio.create_task(task_worker(task_id, task_model.duration))
+    return JSONResponse(content={"task_id": task_id})
+
+
+#            параметр в скобках {} попадает в параметр функции.
+#            Можно указать тип в параметре ф-ии, и он будет преобразован в этот тип
+@app.get("/task/{task_id}", response_model=dict)
+async def check_task(task_id: str):
+    return JSONResponse(content={"status": tasks[task_id]})
